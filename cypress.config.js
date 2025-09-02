029f2e03c05313a50141ac129c3c5c1daeb340bf
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-09-02 13:09:53 -0400

    Mochawesome report added

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,11 @@ module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
   chromeWebSecurity: false,
-
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     baseUrl: 'https://www.pedogist.com/',
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -16,6 +16,7 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 import "cypress-real-events/support";
+import 'cypress-mochawesome-reporter/register';
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
