40816f79aadf4cec404bf8fe67114d64ce7193e3
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-09-23 15:44:06 -0400

    social media link updated

--- cypress/support/social_links.js
+++ cypress/support/social_links.js
@@ -1,6 +1,6 @@
 export const socialLinks = [
-    { social: 'Facebook', index: 0, href: 'https://facebook.com/', iconClass: 'fa-facebook-f' },
+    { social: 'Facebook', index: 0, href: 'https://www.facebook.com/profile.php?id=61571712334833', iconClass: 'fa-facebook-f' },
     { social: 'X', index: 1, href: 'https://x.com/pedogist', iconClass: 'fa-twitter' },
     { social: 'Instagram', index: 2, href: 'https://instagram.com/pedogist/', iconClass: 'fa-instagram' },
-    { social: 'Linkedin', index: 3, href: 'https://linkedin.com/', iconClass: 'fa-linkedin-in' },
+    { social: 'Linkedin', index: 3, href: 'https://www.linkedin.com/company/pedogist/', iconClass: 'fa-linkedin-in' },
 ];
